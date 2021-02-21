4042e388fd3559c57ba813237220cadbe754e830
Tobias Hotz <tobias.hotz@hotmail.de>
2021-02-21 21:27:34 +0100

    Add test for Files.exist in Zipfs

---
+++ src/jmh/java/cpw/mods/performancetests/TestZipfsExists.java
@@ -0,0 +1,36 @@
+package cpw.mods.performancetests;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class TestZipfsExists {
+    private static final Path tmpJar;
+    private static final FileSystem fs;
+
+    static {
+        try {
+            tmpJar = Files.createTempFile("fmltest", "mod.jar");
+            try (InputStream stream = TestZipfsExists.class.getResourceAsStream("/mod.jar")) {
+                Files.copy(stream, tmpJar, StandardCopyOption.REPLACE_EXISTING);
+            }
+            fs = FileSystems.newFileSystem(tmpJar, TestZipfsExists.class.getClassLoader());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Benchmark
+    public void benchmarkFileExists() {
+        if (!Files.exists(fs.getPath("META-INF/mods.toml")))
+            throw new RuntimeException("Expected mods.toml to be present!");
+        if (Files.exists(fs.getPath("META-INF/coremods.json")))
+            throw new RuntimeException("Expected coremods.json to be missing!");
+    }
+}
